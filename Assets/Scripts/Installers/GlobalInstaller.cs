06c7bbc022e38b748ffabf8634a5893295640683
OchPuch <darkkingart@gmail.com>
2024-04-05 15:26:23 +0300

    Zenject

---
+++ Assets/Scripts/Installers/GlobalInstaller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+using Application = UnityEngine.Device.Application;
+
+namespace Installers
+{
+    public class GlobalInstaller : MonoInstaller
+    {
+        
+        public override void InstallBindings()
+        {
+            Debug.Log("GlobalInstaller Installed");
+            Application.targetFrameRate = 240;
+        }
+    }
+}
