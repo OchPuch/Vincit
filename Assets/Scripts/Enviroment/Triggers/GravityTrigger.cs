e4e47faae77d335010a79eaf686943b1b639d626
OchPuch <darkkingart@gmail.com>
2024-04-10 10:59:00 +0300

    Outline Fix

--- Assets/Scripts/Enviroment/Triggers/GravityTrigger.cs
+++ Assets/Scripts/Enviroment/Triggers/GravityTrigger.cs
@@ -1,5 +1,7 @@
+using System;
 using NaughtyAttributes;
 using UnityEngine;
+using CharacterController = Player.CharacterController;
 
 namespace Enviroment.Triggers
 {
@@ -7,6 +9,7 @@ namespace Enviroment.Triggers
     {
         [SerializeField] private Vector3 gravity;
         [SerializeField] private GravityType gravityType;
+        [SerializeField] private bool prioritizeIfOverlapping;
 
         [Button("Rotate gravity to Down")]
         private void RotateGravityDown()
@@ -19,29 +22,46 @@ namespace Enviroment.Triggers
         {
             gravity = transform.up * gravity.magnitude;
         }
+        
+        private void SetGravityForPlayer(CharacterController player)
+        {
+            switch (gravityType)
+            {
+                case GravityType.Center:
+                    //Calculate the direction from the object to the center of the planet
+                    var direction = (transform.position - player.transform.position).normalized;
+                    player.SetGravity(direction * gravity.magnitude);
+                    break;
+                case GravityType.Custom:
+                    player.SetGravity(gravity);
+                    break;
+            }
+        }
+        
     
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                var obj = other.GetComponent<Player.CharacterController>();
+                var obj = other.GetComponent<CharacterController>();
                 if (obj)
                 {
-                    switch (gravityType)
-                    {
-                        case GravityType.Center:
-                            //Calculate the direction from the object to the center of the planet
-                            var direction = (transform.position - other.transform.position).normalized;
-                            obj.SetGravity(direction * gravity.magnitude);
-                            break;
-                        case GravityType.Custom:
-                            obj.SetGravity(gravity);
-                            break;
-                    }
+                    SetGravityForPlayer(obj);
                 }
             }
         }
-    
+        
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (!prioritizeIfOverlapping || !other.CompareTag("Player")) return;
+            var obj = other.GetComponent<CharacterController>();
+            if (obj)
+            {
+                SetGravityForPlayer(obj);
+            }
+        }
+
         private enum GravityType
         {
             Center,
