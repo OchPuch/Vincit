3344b950cfada781c01bd8eb960773c1d160a90d
OchPuch <darkkingart@gmail.com>
2024-07-08 04:51:01 +0300

    Wall Detection Fix

--- Assets/Scripts/Player/AdditionalPhysics/WallDetector.cs
+++ Assets/Scripts/Player/AdditionalPhysics/WallDetector.cs
@@ -8,7 +8,7 @@ namespace Player.AdditionalPhysics
 {
     public class WallDetector : MonoBehaviour
     {
-        [SerializeField] private CapsuleCollider nearCollider;
+        [SerializeField] private BoxCollider nearCollider;
         private PlayerData _playerData;
         private int _objectCount;
   
@@ -16,43 +16,14 @@ namespace Player.AdditionalPhysics
         public void Init(PlayerData data, StateMachine.StateMachine stateMachine)
         {
             _playerData = data;
-            WallCheck();
-            stateMachine.StateEntered += OnStateEntered;
-        }
-
-        private void OnStateEntered(IState obj)
-        {
-            if (obj is DefaultAirborneState)
-            {
-                WallCheck();
-            }
         }
         
-        private void WallCheck()
-        {
-            //Overlap sphere same as collider and check if theres any level layer object
-            _objectCount = 0;
-            _playerData.isNearWall = false;
-            _playerData.wallNormal = Vector3.zero;
-            _playerData.lastWallJumpCollider = null;
-            
-            //Create overlapp sphere
-            Collider[] colliders = new Collider[1];
-            _objectCount = Physics.OverlapSphereNonAlloc(transform.position, nearCollider.radius, colliders, _playerData.levelLayerMask); 
-            if (_objectCount > 0)
-            {
-                _playerData.isNearWall = true;
-                _playerData.lastWallJumpCollider = colliders[0];
-            }
-        }
-        
-        private void OnTriggerEnter(Collider other)
+        private void OnTriggerStay(Collider other)
         {
             if (LayerUtils.IsInLayerMask(other.gameObject.layer, _playerData.levelLayerMask))
             {
                 _playerData.lastWallJumpCollider = other;
                 _playerData.isNearWall = true;
-                _objectCount++;
             }
         }
         
@@ -60,11 +31,9 @@ namespace Player.AdditionalPhysics
         {
             if (LayerUtils.IsInLayerMask(other.gameObject.layer, _playerData.levelLayerMask))
             {
-                _objectCount--;
-                if (_objectCount > 0) return;
                 _playerData.isNearWall = false;
                 _playerData.wallNormal = Vector3.zero;
-                _objectCount = 0;
+                _playerData.lastWallJumpCollider = null;
             }
         }
     }
